38f7042eb74cd31fb8150da3bdd9ad6986602a13
Devkant Gorai <51082429+CSElonewolf@users.noreply.github.com>
2020-03-23 13:13:49 +0530

    Added Peak_Value.c

---
+++ Solutions in C/Peak_Value.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+   for(int i=0;i<n-1;i++)
+   {
+       scanf("%d",&arr[i]);
+   }
+   for(int i=0;i<n-1;i++)
+   {
+       if(arr[i]>arr[i+1]) {
+           printf("%d", arr[i]);
+           break;
+       }
+   }
+}
+
+/*
+  8-->Array size
+1
+2
+3
+4
+5
+6--->Peak value 
+4
+ 
+ 
+6-->output of peak value
+  */
